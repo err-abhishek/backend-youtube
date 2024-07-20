67e19b5a6c4b2116018544a2f7a3de91c883c6a1
Abhishek Agrahari <abhishek737333@gmail.com>
2024-07-20 14:13:11 +0530

    Login and logout Done

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,12 @@
 const asyncHandler = (reqHandler) => {
-  (req, res, next) => {
+ return (req, res, next) => {
     Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
 export { asyncHandler };
 
-// const asyncHandler = (fn) => async (req,res,next) =>{
+// const asyncHandler = (fn) => {async (req,res,next)} =>{
 //     try {
 //         await fn(req,res,next)
 //     } catch (error) {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import fs from "fs";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -15,9 +15,11 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file has been uploaded successfully
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
+    console.log("hifne");
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
